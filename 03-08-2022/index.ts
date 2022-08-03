19faea4a383f905970277c82e3684b72da1f3ea7
Me5uT <me5ut@windowslive.com>
2022-08-03 22:32:16 +0300

    challenge 03-08-2022 done

---
+++ 03-08-2022/index.ts
@@ -0,0 +1,43 @@
+const isPalindrome = (word: string): boolean => {
+  // parçala, ters çevir ve birleştir
+  const reversedWord = word.split("").reverse().join("");
+
+  return reversedWord == word;
+};
+
+const reverseText = (text: string) => {
+  // textin başındaki ve sondaki boşlukları kaldır, lower case yap
+  const trimedAndLowerCaseText = text.trim().toLowerCase();
+  //  kelime mi cümle mi kontrolü
+  const isWord = trimedAndLowerCaseText.split(" ").length < 2;
+
+  if (isWord) {
+    // kelimelerin palindrome kontrolü
+    const isWordPalindrome = isPalindrome(trimedAndLowerCaseText);
+
+    if (isWordPalindrome) {
+      console.log(
+        `${trimedAndLowerCaseText}, bir kelimedir ve palindrome'dur.`
+      );
+    } else {
+      const reversedWord = trimedAndLowerCaseText.split("").reverse().join("");
+      console.log(
+        `${trimedAndLowerCaseText}, bir kelimedir ve tersi ${reversedWord}'dir.`
+      );
+    }
+  } else {
+    // text kelime değil ise; boşluklara göre parçala, her kelimeyi ters çevir, ve birleştir
+    const reversedText = trimedAndLowerCaseText
+      .split(" ")
+      .map((word) => word.split("").reverse().join(""))
+      .join(" ");
+
+    console.log(
+      `${trimedAndLowerCaseText}, bir kelime değildir ve tersi ${reversedText}'dir.`
+    );
+  }
+};
+
+reverseText("this is a sentence");
+reverseText("this ");
+reverseText(" this");
---
+++ 03-08-2022/readme.md
@@ -0,0 +1,4 @@
+# case
+
+Verilen texti tersine çeviren bir fonksiyon.
+Not: Text, kelime ise palindrome mu diye kontrol et.
