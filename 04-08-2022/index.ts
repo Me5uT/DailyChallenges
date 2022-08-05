38ffe2eb71234be8e254e153b21a4dac6afa5be7
Me5uT <me5ut@windowslive.com>
2022-08-05 10:49:30 +0300

    04-08-2022 made code refactor

--- 04-08-2022/index.ts
+++ 04-08-2022/index.ts
@@ -2,32 +2,21 @@ const someNumbers: number[] = [1, 2, 14, 5, 7, 9];
 
 const findLostNumbers = (numbers: number[]): void => {
   const lostNumbers: number[] = [];
-  let min: number = 999;
-  let max: number = 0;
-
-  // // array in içindeki sayıların min ve max ı bul
-  // V1
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > max) max = numbers[i];
-    if (numbers[i] < min) min = numbers[i];
-  }
-
-  // // V2
-  //   numbers.forEach((num: number) => {
-  //     if (num > max) max = num;
-  //     if (num < min) min = num;
-  //   });
-
-  console.log("min", min, "max", max);
+  let allNumbers: number[] = [];
 
   // min den max a kadar bir döngüde array in içinde olmayan sayıları bul
-  for (let i = min; i < max; i++) {
+  for (let i = Math.min(...numbers); i < Math.max(...numbers); i++) {
     if (!numbers.includes(i)) {
       lostNumbers.push(i);
     }
   }
 
   console.log("lostNumbers", lostNumbers);
+
+  allNumbers = [...numbers, ...lostNumbers].sort((a, b) => {
+    return a - b;
+  });
+  console.log("allNumbers", allNumbers);
 };
 
 findLostNumbers(someNumbers);
